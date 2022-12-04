2d6b8d20f31c2132450bc7edcb5717cc3dd33011
sama85 <samayasser8520@gmail.com>
2022-12-04 16:59:11 +0200

    encapuslated live data in Game fragment

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -8,10 +9,17 @@ const val GAME_VIEW_MODEL = "GameViewModel"
 
 class GameViewModel : ViewModel() {
 
-    // The current word
-    val word = MutableLiveData<String>()
-    // The current score
-    val score = MutableLiveData<Int>()
+    // Encapuslate live data
+    //mutable should be private and returned as Live data to avoid changing it anywhere outside viewModel
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
+
+    private val _score = MutableLiveData<Int>()
+    //returns score as live data when accessed to only observe/read
+    val score : LiveData<Int>
+        get() = _score
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -21,8 +29,8 @@ class GameViewModel : ViewModel() {
         /**
          * Resets the list of words and randomizes the order once
          */
-        score.value = 0
-        word.value = ""
+        _score.value = 0
+        _word.value = ""
         resetList()
         nextWord()
     }
@@ -61,17 +69,17 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     fun onSkip() {
-        score.value = score.value?.minus(1)
+        _score.value = score.value?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = score.value?.plus(1)
+        _score.value = score.value?.plus(1)
         nextWord()
     }
     //view model is destroyed when associated activity/frag is completely
